c3012d4b220955e7a291f13539510ca80b07073f
markxoe <mark@oude-elberink.de>
2021-02-02 18:02:08 +0100

    :sparkles: Secure Upload: Adding Secrets

--- src/secrets.js
+++ src/secrets.js
@@ -2,8 +2,16 @@ const insertApiKey = process.env.insertApiKey;
 const deleteApiKey = process.env.deleteApiKey;
 const usersApiSecret = process.env.usersApiSecret;
 
+const v2 = {
+  privateUploadKeyBase64: process.env.v2_private_upload_key_base64,
+  publicUploadKeyBase64: process.env.v2_public_upload_key_base64,
+  publicUploadKeyKey: process.env.v2_private_upload_key_key,
+  totpSecret: process.env.v2_totp_secret,
+};
+
 module.exports = {
   insertApiKey,
   deleteApiKey,
   usersApiSecret,
+  v2,
 };
